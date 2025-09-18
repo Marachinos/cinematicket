12f322cc408570a79f642678065198a78828eeae
Marachinos <stallnysater@gmail.com>
2025-09-18 21:03:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace biobiljett
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                //Filmer, priser, rabatt och moms (Movies, prices, discount and taxes
+            string[] movies = { "Super Mario Bros. Filmen", "Dumma Mig 3", "The BeeKeeper", "The Old Guard 2" };
+            double[] prices = { 120, 100, , 180, 190 };
+            const double TAX = 0.06; //6% moms
+            const double STUDENT_DISCOUNT = 0.15;//15% rabatt för studenter
+            const string CURRENCY = "SEK";
+        }
+    }
+}
